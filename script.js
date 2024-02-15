dc17997a969054aef68da097d1fc118a737a4edb
Christian Arlu Laderas <christianarlu@Xubuntu2024.myguest.virtualbox.org>
2024-02-15 15:04:54 +0800

    Create rps-ui branch and add GUI

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 /*
-Pseudocode
-
-*No GUI*
-*Inputs are 'Rock' , 'Paper', and 'Scissors' which are obtained via prompt method.*
-
-1. User provides inputs in the console log
-2. Pass inputs to function getComputerChoice, with the parameters
-    - playerSelection (case insensitive)
-    - computerSelection
-3. Process string to declare winner based on the values of playerSelection and computerSelection
+const btnRock = document.querySelector('btn-rock'); 
+const btnPaper = document.querySelector('btn-paper');
+const btnScissors = document.querySelector('btn-scissors');
 */
 
 function getComputerChoice (computerSelection){
@@ -19,7 +12,6 @@ function getComputerChoice (computerSelection){
 }
 
 let playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
         return "It's a tie!";
@@ -40,37 +32,3 @@ let playRound = (playerSelection, computerSelection) => {
     }
 };
 
-let playGame = function () {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (i = 1; i <= 5; i++) {
-        let playerSelection = prompt("Enter Rock, Paper, or Scissors");
-        let roundText = playRound(playerSelection, getComputerChoice()); 
-
-        roundText.substring(0, 5) === "You W" ? ++playerScore : 
-        roundText.substring(0, 5) === "You L" ? ++computerScore : playerScore = playerScore;
-
-        console.log(`Round ${i}: ${roundText}`);
-
-    }
-
-      if (playerScore > computerScore) {
-        console.log(`You Win! Congrats! || Player score: ${playerScore} || Computer score: ${computerScore}`);
-    } else if (playerScore == computerScore) {
-        console.log(`It's a tie! || Player score: ${playerScore} || Computer score: ${computerScore}`);
-
-    } else {
-        console.log(`You Lose! || Player score: ${playerScore} || Computer score: ${computerScore}`);
-        
-        confirm("Try again?") ? playGame() :  console.log("Thanks for playing!");
-    }
-
-
-};
-
-console.info("EYES HERE! Enter 'playGame();' to play.'");
-
-
-//const computerSelection = getComputerChoice();
-//console.log(playRound(playerSelection, getComputerChoice()));
